620216676e3ee6464e273ce325af070a7ee23696
Bruce Zawadzki <bruce.zawadzki@gmail.com>
2025-11-30 20:35:15 -0600

    Added Audio Clip Visualizer

--- src/deluge/gui/views/audio_clip_view.cpp
+++ src/deluge/gui/views/audio_clip_view.cpp
@@ -31,6 +31,7 @@
 #include "gui/waveform/waveform_renderer.h"
 #include "hid/buttons.h"
 #include "hid/display/display.h"
+#include "hid/display/visualizer.h"
 #include "hid/led/indicator_leds.h"
 #include "hid/led/pad_leds.h"
 #include "hid/matrix/matrix_driver.h"
@@ -93,6 +94,11 @@ void AudioClipView::focusRegained() {
 }
 
 void AudioClipView::renderOLED(deluge::hid::display::oled_canvas::Canvas& canvas) {
+	// Check if visualizer should be displayed in clip view
+	if (deluge::hid::display::Visualizer::potentiallyRenderVisualizer(canvas)) {
+		return;
+	}
+
 	view.displayOutputName(getCurrentOutput(), false, getCurrentClip());
 }
 
