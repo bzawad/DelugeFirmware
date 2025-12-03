1ec5ec3b36a8e438ae4368d1f364e9a9ff7e2ddd
Bruce Zawadzki <bruce.zawadzki@gmail.com>
2025-12-03 01:13:08 -0600

    Cleanup

--- src/deluge/gui/views/session_view.cpp
+++ src/deluge/gui/views/session_view.cpp
@@ -1864,7 +1864,7 @@ void SessionView::renderOLED(deluge::hid::display::oled_canvas::Canvas& canvas) 
 		return;
 	}
 
-	// Check if visualizer should be displayed (same conditions as VU meter)
+	// Check if visualizer should be displayed
 	if (deluge::hid::display::Visualizer::potentiallyRenderVisualizer(canvas)) {
 		return;
 	}
